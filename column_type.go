6d0114ee02f9f8d5c86cca04c5f8d8ecda7ef9e6
can.erdogan <can.erdogan@ekinokssoftware.com>
2024-05-15 13:30:49 +0300

    refactor: column type conversions added

--- column_type.go
+++ column_type.go
@@ -1,14 +1,40 @@
 package go_migrator
 
+import "fmt"
+
 type ColumnType int32
 
 const (
-	ColumnTypeString    ColumnType = 1
-	ColumnTypeInteger   ColumnType = 2
-	ColumnTypeUUID      ColumnType = 3
-	ColumnTypeBoolean   ColumnType = 4
-	ColumnTypeFloat     ColumnType = 5
-	ColumnTypeDouble    ColumnType = 6
-	ColumnTypeDecimal   ColumnType = 7
-	ColumnTypeTimestamp ColumnType = 8
+	ColumnTypeSerial    ColumnType = 1
+	ColumnTypeString    ColumnType = 2
+	ColumnTypeInteger   ColumnType = 3
+	ColumnTypeUUID      ColumnType = 4
+	ColumnTypeBoolean   ColumnType = 5
+	ColumnTypeFloat     ColumnType = 6
+	ColumnTypeDouble    ColumnType = 7
+	ColumnTypeDecimal   ColumnType = 8
+	ColumnTypeTimestamp ColumnType = 9
 )
+
+func (c ColumnType) toString(length int32) string {
+	switch c {
+	case ColumnTypeSerial:
+		return "SERIAL"
+	case ColumnTypeString:
+		return "VARCHAR(" + fmt.Sprintf("%d", length) + ")"
+	case ColumnTypeUUID:
+		return "UUID"
+	case ColumnTypeInteger:
+		return "INT"
+	case ColumnTypeBoolean:
+		return "BOOLEAN"
+	case ColumnTypeFloat:
+		return "FLOAT"
+	case ColumnTypeDouble:
+		return "DOUBLE"
+	case ColumnTypeTimestamp:
+		return "TIMESTAMP"
+	}
+
+	return ""
+}
