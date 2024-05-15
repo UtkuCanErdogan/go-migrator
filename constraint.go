0663206452d274375d1939a3a1726c7389881e35
can.erdogan <can.erdogan@ekinokssoftware.com>
2024-05-15 13:31:00 +0300

    refactor: constraint conversions added

--- constraint.go
+++ constraint.go
@@ -1,11 +1,39 @@
 package go_migrator
 
-type Constraint int32
+type Constraint struct {
+	name  string
+	cType ConstraintType
+}
+
+type ConstraintType int32
 
 const (
-	AutoIncrementConstraint Constraint = 1
-	NotNullConstraint       Constraint = 2
-	UniqueConstraint        Constraint = 3
-	PrimaryKeyConstraint    Constraint = 4
-	ForeignKeyConstraint    Constraint = 5
+	NotNullConstraint    ConstraintType = 1
+	UniqueConstraint     ConstraintType = 2
+	PrimaryKeyConstraint ConstraintType = 3
+	ForeignKeyConstraint ConstraintType = 4
 )
+
+func (c ConstraintType) toString() string {
+	switch c {
+	case NotNullConstraint:
+		return "NOT NULL"
+	case UniqueConstraint:
+		return "UNIQUE"
+	case PrimaryKeyConstraint:
+		return "PRIMARY KEY"
+	}
+
+	return ""
+}
+
+func (c ConstraintType) ToLower() string {
+	switch c {
+	case UniqueConstraint:
+		return "unique"
+	case PrimaryKeyConstraint:
+		return "primary_key"
+	}
+
+	return ""
+}
